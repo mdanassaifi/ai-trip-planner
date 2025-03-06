07bbfb4f44ff82da71d484a0b092ecd82224e823
mdanassaifi <cscanas533@gmail.com>
2025-03-06 23:31:46 +0530

    bugfixed

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
   return (
     <>
       <div>
+        <h1> Trip Planner</h1>
         <Button>Click me</Button>
         <h2 className='m-0 mt-0 '> cow is {count}</h2>
         <button onClick={() => setCount(count + 1)}>+</button>
