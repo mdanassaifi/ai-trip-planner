a416bf087cc5f1d266c80c6879eb2d5579234b4a
mdanassaifi <cscanas533@gmail.com>
2025-03-07 23:10:51 +0530

    today

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ function App() {
     <>
       <div>
         <h1> Trip Planner</h1>
+        <p>This is a Trip Planner App</p>
         <Button>Click me</Button>
         
         <button onClick={() => setCount(count + 1)}>+</button>
