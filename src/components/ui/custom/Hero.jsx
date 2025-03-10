a419dbd8a566996209724bf0fd4822731160618e
mdanassaifi <cscanas533@gmail.com>
2025-03-10 05:43:02 +0530

    addclr

--- src/components/ui/custom/Hero.jsx
+++ src/components/ui/custom/Hero.jsx
@@ -6,7 +6,7 @@ function Hero() {
   return (
     <div className='flex flex-col items-center mx-56 gap-9'>
       <h1 className='font-extrabold text-[50px] text-center mt-16'>
-        <span className='text-[#f56551]'> Discover Your Next Adventure with AI:</span> Personalized Iteneries at Your Fingertips
+        <span className='text-[#f56551]'> Discover Your Next Adventure with AI:</span> Personalized <span className='text-[#311b92]'>Iteneries at Your Fingertips</span>
       </h1>
       <p className='text-xl text-gray-500 text-center'> Your personal trip planner and travel curator, creating custom itineraries tailored to your interests and budget.</p>
 
